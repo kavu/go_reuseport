ecd799e73534f83643e4c396cd64594e16341e7b
Max Riveiro <kavu13@gmail.com>
2016-08-19 11:48:12 +0300

    Add few build tags
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- reuseport.go
+++ reuseport.go
@@ -1,3 +1,5 @@
+// +build linux darwin dragonfly freebsd netbsd openbsd
+
 // Copyright (C) 2016 Max Riveiro
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
