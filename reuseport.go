8c284b7067d8bb6a6292b665f55d665df97c4b6e
Max Riveiro <kavu13@gmail.com>
2016-08-22 11:56:44 +0300

    Tune godoc and licenses comments
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- reuseport.go
+++ reuseport.go
@@ -6,7 +6,8 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Package reuseport provides a function that returns a net.Listener powered by a net.FileListener with a SO_REUSEPORT option set to the socket.
+// Package reuseport provides a function that returns a net.Listener powered
+// by a net.FileListener with a SO_REUSEPORT option set to the socket.
 package reuseport
 
 import (
@@ -20,7 +21,8 @@ const fileNameTemplate = "reuseport.%d.%s.%s"
 
 var errUnsupportedProtocol = errors.New("only tcp, tcp4, tcp6, udp, udp4, udp6 are supported")
 
-// getSockaddr parses protocol and address and returns implementor syscall.Sockaddr: syscall.SockaddrInet4 or syscall.SockaddrInet6.
+// getSockaddr parses protocol and address and returns implementor
+// of syscall.Sockaddr: syscall.SockaddrInet4 or syscall.SockaddrInet6.
 func getSockaddr(proto, addr string) (sa syscall.Sockaddr, soType int, err error) {
 	switch proto {
 	case "tcp", "tcp4", "tcp6":
